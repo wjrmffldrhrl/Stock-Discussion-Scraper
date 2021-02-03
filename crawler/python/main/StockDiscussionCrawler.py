ee65383ed454aeb5de72955594b0c78bb27be9a7
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2021-02-03 23:28:38 +0900

    error in requests [requests.exceptions.ConnectionError: HTTPSConnectionPool(host='finance.naver.comhttps', port=443): Max retries exceeded with url: //finance.naver.com/item/board_read.nhn?code=005930&nid=162281513&st=&sw=&page=1 (Caused by NewConnectionError('<urllib3.connection.HTT

--- crawler/python/main/StockDiscussionCrawler.py
+++ crawler/python/main/StockDiscussionCrawler.py
@@ -1,5 +1,10 @@
-import requests
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+from Discussion import Discussion
 from bs4 import BeautifulSoup
+import requests
+import time
 
 
 class StockDiscussionCrawler(object):
@@ -22,31 +27,55 @@ class StockDiscussionCrawler(object):
 
         return discussion_url
 
-
     def __get_discussion(self, discussion_url):
         target_url = self.__main_url + discussion_url
+        # target_url = "https://finance.naver.com/item/board_read.nhn?code=005930&nid=162275548&st=&sw=&page=1"
         headers = {"referer": target_url}
         request = requests.get(target_url, headers=headers)
 
-
         bs = BeautifulSoup(request.text, "html.parser")
 
+        # print(bs.contents)
         content_table = bs.find("table", {"summary": "게시판 글 본문보기"})
 
-        title = content_table.find("strong").contents
+        title = content_table.find("strong").text
+
         content = content_table.find("div", {"id": "body"}).contents
 
         next_previous_table = bs.find("table", {"summary": "이전, 다음 글 목록"})
 
-        print(next_previous_table)
-        # print("title: ", title)
-        # print("content : ", content)
+        # 이전 다음 글 조회
+        # 처음과 마지막 a 태그의 속성이 타겟이다
+        # 현재 게시글까지 총 3개가 나오지만 가끔 답글이 달리는 경우
+        # 4개 이상이 되는 경우도 있다.
+        next_discussion_url = ""
 
-    def get(self):
+        a_tags = next_previous_table.findAll("a")
+        if "title" in a_tags[0].attrs:
+            next_discussion_url = a_tags[0].attrs["href"]
+
+        previous_discussion_url = a_tags[len(a_tags) - 1].attrs["href"]
+
+        return Discussion(target_url, title, content, "comment", previous_discussion_url, next_discussion_url)
+
+    def crawling_discussions(self, quantity):
         discussion_url = self.__get_start_discussion_url()
-        self.__get_discussion(discussion_url)
+
+        count = 0
+        while count < quantity:
+            time.sleep(10)
+            discussion = self.__get_discussion(discussion_url)
+
+            # if not discussion.has_next():
+            #     print("no next discussion...")
+            #     continue
+
+            discussion_url = self.__main_url + "/item/" + discussion.get_previous_discussion_url()
+            print(discussion.get_title())
+            print(discussion_url)
+            count += 1
 
 
 if __name__ == '__main__':
     samsungDiscussionCrawler = StockDiscussionCrawler("005930")
-    samsungDiscussionCrawler.get()
+    samsungDiscussionCrawler.crawling_discussions(10)
