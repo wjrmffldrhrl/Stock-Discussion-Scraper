a8501cad325c5b0d3c5fd500cfb4064881802665
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2021-02-13 00:43:26 +0900

    google driver file upload, create directory

--- crawler/scala/src/main/scala/crawler/manager/file/GoogleDriveManager.java
+++ crawler/scala/src/main/scala/crawler/manager/file/GoogleDriveManager.java
@@ -4,14 +4,19 @@ import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.services.drive.model.FileList;
 
 import java.io.*;
+import java.security.GeneralSecurityException;
 import java.util.*;
 
 public class GoogleDriveManager {
@@ -50,6 +55,88 @@ public class GoogleDriveManager {
 
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    public static String findDirectory(String fileName) throws GeneralSecurityException, IOException {
+        StringBuilder resultStr = new StringBuilder();
+        Drive service = getService();
+
+
+
+        FileList files = service.files().list()
+                .setQ("mimeType='application/vnd.google-apps.folder' and trashed = false and name='" + fileName + "'")
+                .setSpaces("drive")
+                .setFields("nextPageToken, files(id, name)")
+                .execute();
+
+
+        for (com.google.api.services.drive.model.File file : files.getFiles()) {
+            resultStr.append(",").append(file.getId());
+        }
+
+        if (resultStr.length() == 0) {
+            return "";
+        }
+
+        return resultStr.substring(1);
+    }
+
+    public static String createDirectory(String directoryName) throws GeneralSecurityException, IOException {
+        String discussionDirectoryId = "1NvZhfU-8D7SQurvt0CZtP2TA-Er7JADs";
+        com.google.api.services.drive.model.File fileMetadata = new com.google.api.services.drive.model.File();
+        fileMetadata.setName(directoryName);
+        fileMetadata.setMimeType("application/vnd.google-apps.folder");
+        fileMetadata.setParents(Collections.singletonList(discussionDirectoryId));
+
+
+        Drive service = getService();
+
+
+        com.google.api.services.drive.model.File result = service.files().create(fileMetadata)
+                .setFields("id")
+                .execute();
+
+
+
+
+        return result.getId();
+    }
+
+    private static Drive getService() throws GeneralSecurityException, IOException {
+        NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+
+
+        return new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+                GoogleDriveManager.getCredentials(HTTP_TRANSPORT)).setApplicationName("stock-discussion-analysis").build();
+    }
+
+    public static String uploadFile(File targetFile) throws IOException, GeneralSecurityException, InterruptedException {
+
+        String itemCode = targetFile.getParentFile().getName();
+
+        String itemDirectoryId = findDirectory(itemCode);
+
+        if (itemDirectoryId.contains(",")) {
+            throw new RuntimeException("Has multi item directory");
+        } else if (itemDirectoryId.length() < 1) {
+            itemDirectoryId = createDirectory(itemCode);
+        }
+
+        com.google.api.services.drive.model.File fileMetadata = new com.google.api.services.drive.model.File();
+        fileMetadata.setName(targetFile.getName());
+        fileMetadata.setMimeType("application/vnd.google-apps.spreadsheet");
+        fileMetadata.setParents(Collections.singletonList(itemDirectoryId));
+
+        FileContent mediaContent = new FileContent("text/csv", targetFile);
+
+        Drive service = getService();
+
+        com.google.api.services.drive.model.File result = service.files().create(fileMetadata, mediaContent)
+                .setFields("id")
+                .execute();
+
+        return result.getId();
+
     }
 
     public static JsonFactory getJsonFactory() { return JSON_FACTORY; }
--- crawler/scala/src/test/scala/crawler/GoogleDriveTest.scala
+++ crawler/scala/src/test/scala/crawler/GoogleDriveTest.scala
@@ -26,28 +26,16 @@ class GoogleDriveTest extends FunSuite{
 
 
   test("test file upload ") {
-    val fileMetadata = new com.google.api.services.drive.model.File()
+    println(GoogleDriveManager.uploadFile(new java.io.File("test/005930/2021_02_10.csv")))
 
-    val directoryId = "1NvZhfU-8D7SQurvt0CZtP2TA-Er7JADs"
+  }
 
+  test("test create directory") {
+    println(GoogleDriveManager.createDirectory("005930"))
+  }
 
-    fileMetadata.setName("2021_02_10.csv")
-    fileMetadata.setMimeType("application/vnd.google-apps.spreadsheet")
-
-    val testFile = new java.io.File("test/005930/2021_02_10.csv")
-    val mediaContent = new FileContent("text/csv", testFile)
-
-    val HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport()
-    val service = new Drive.Builder(HTTP_TRANSPORT, GoogleDriveManager.getJsonFactory,
-      GoogleDriveManager.getCredentials(HTTP_TRANSPORT)).setApplicationName("stock-discussion-analysis").build()
-
-    val result = service.files().create(fileMetadata, mediaContent)
-      .setFields("id")
-      .execute()
-
-    println(result.getId)
-
-
+  test("test find File") {
+    println(GoogleDriveManager.findDirectory("005930"))
   }
 
 }
