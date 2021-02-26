cbcd44c73025603dbbdcceba8fbc931c1a7140fa
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2021-02-26 23:52:57 +0900

    crawler need to change kotlin

--- src/main/scala/crawler/manager/file/GoogleFileUploader.scala
+++ src/main/scala/crawler/manager/file/GoogleFileUploader.scala
@@ -6,20 +6,16 @@ import java.time.LocalDateTime
 /**
  * 파일 처리 클래스
  *
- * @param itemCode 해당 파일 처리 클래스가 사용되는 종목 코드
+ * @param itemCode  해당 파일 처리 클래스가 사용되는 종목 코드
  * @param cycleTime 해당 파일 처리 클래스가 동작하는 주기
  */
 class GoogleFileUploader(itemCodes: Array[String], inputCycleTime: Int) extends FileManager {
 
 
-  private val targetDirectoryPath = "discussion/" + itemCode
-  private val targetDirectory = new File(targetDirectoryPath)
-
-
-  private var oldFileName = "init.csv"
+  private var oldFileNames = itemCodes.map(itemCode => (itemCode, "init")).toMap
   override val cycleTime: Int = inputCycleTime
 
-  private def uploadFile(targetFileName: String): Unit ={
+  private def uploadFile(targetFileName: String): Unit = {
 
     val targetFile = new File(targetDirectoryPath + "/" + oldFileName)
 
@@ -35,16 +31,23 @@ class GoogleFileUploader(itemCodes: Array[String], inputCycleTime: Int) extends 
 
   override def work(): Unit = {
 
-    val otherFiles = targetDirectory.list().filter(file => file.endsWith(".csv")).filter(file => !file.equals(oldFileName))
+    itemCodes.foreach(itemCode => {
 
-    if (!otherFiles.isEmpty) {
-      val latestFileName = otherFiles.head
+      val targetDirectoryPath = "discussion/" + itemCode
+      val targetDirectory = new File(targetDirectoryPath)
+      val otherFiles = targetDirectory.list().filter(file => file.endsWith(".csv")).filter(file => !file.equals(oldFileName))
 
-      uploadFile(oldFileName)
+      if (!otherFiles.isEmpty) {
+        val latestFileName = otherFiles.head
+
+        uploadFile(oldFileName)
 
 
-      oldFileName = latestFileName
-    }
+        oldFileName = latestFileName
+      }
+    })
+
+
   }
 
 
