558de537260a3775a47d0a1d490d03e30cc4cd38
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2021-03-02 12:47:31 +0900

    update discussion to csv

--- src/main/kotlin/com/stock/StockDiscussion.kt
+++ src/main/kotlin/com/stock/StockDiscussion.kt
@@ -9,6 +9,15 @@ class StockDiscussion(
     val nextDiscussionUrl: String
 ) {
 
-    fun toCsv() = "$date,$title,$content,$url,$previousDiscussionUrl,$nextDiscussionUrl"
+    fun toCsv(): String {
+        val csvTitle = title.replace(",", " ")
+        val csvContent = content.replace(",", " ")
+        val csvUrl = url.replace(",", " ")
+        val csvPreviousDiscussionUrl = previousDiscussionUrl.replace(",", " ")
+        val csvNextDiscussionUrl = nextDiscussionUrl.replace(",", " ")
+
+
+        return "$date,$csvTitle,$csvContent,$csvUrl,$csvPreviousDiscussionUrl,$csvNextDiscussionUrl"
+    }
     override fun toString() = "url : [$url] \n title : [$title] \n content: [$content] \n date: [$date] \n previousDiscussionUrl : [$previousDiscussionUrl] \n nextDiscussionUrl : [$nextDiscussionUrl]"
 }
